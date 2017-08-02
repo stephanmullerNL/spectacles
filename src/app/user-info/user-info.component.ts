d1b1ff35af6a157c21e5ee3aee1e38d5af339c3f
Stephan Muller <stephan@frontmen.nl>
2017-08-02 13:45:53 +0200

    user-info styling

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -2,17 +2,21 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-user-info',
-    styleUrls: ['./user-info.component.css'],
+    styleUrls: ['./user-info.component.scss'],
     templateUrl: './user-info.component.html'
 })
 export class UserInfoComponent implements OnInit {
-    @Input() user: string;
+    @Input() user;
 
     constructor() {
     }
 
     ngOnInit() {
         console.log(this.user);
+    }
+
+    getPictureUrl(): string {
+        return `url(${this.user.profile.profile_image})`;
     }
 
 }
