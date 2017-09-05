bf71f8daf138a2143ba608ecd778c2dfb36ea69b
Stephan Muller <stephan@frontmen.nl>
2017-09-05 17:16:10 +0200

    Replace userLink pipe with component

--- src/app/common/pipes/user-link.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UserLinkPipe } from './user-link.pipe';
-
-describe('UserLinkPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UserLinkPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
